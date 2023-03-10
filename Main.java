82520fe21da39fc62cfb9d7138c4c4f7b5b52c61
Angela Jobi <123100397+abj5298@users.noreply.github.com>
2023-03-10 16:20:56 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,56 +4,56 @@ public class Main {
     public static void main(String[] args) {
 
 
-       // Item item = new Item("Item name", "Item description", 10);
+        Item item = new Item("Item name", "Item description", 10);
 
-       // Gold goldBar = new Gold(10);
+        Gold goldBar = new Gold(10);
 
-       // Weapon weapon = new Weapon("super weapon", "super 2 weapon", 10, 50);
-       // weapon.setDamage(40);
-       // int dvalue = weapon.getDamage();
+        Weapon weapon = new Weapon("super weapon", "super 2 weapon", 10, 50);
+        weapon.setDamage(40);
+        int dvalue = weapon.getDamage();
 
-       // Sword magicSword = new Sword();
+        Sword magicSword = new Sword();
 
-       // Pillow softPillow = new Pillow();
+        Pillow softPillow = new Pillow();
 
-       // Mace bigMace = new Mace();
+        Mace bigMace = new Mace();
 
-       // Arrow arrow = new Arrow();
+        Arrow arrow = new Arrow();
 
-       // Enemy enemy = new Enemy("Enemy name", 10, 10);
+        Enemy enemy = new Enemy("Enemy name", 10, 10);
 
-       // Ogre greenOgre = new Ogre();
-       // Ogre redOgre = new Ogre();
+        Ogre greenOgre = new Ogre();
+        Ogre redOgre = new Ogre();
 
-        //Zombie slowZombie = new Zombie();
-        //Zombie fastZombie = new Zombie();
+        Zombie slowZombie = new Zombie();
+        Zombie fastZombie = new Zombie();
 
-       // Dog spotDog = new Dog();
+        Dog spotDog = new Dog();
 
-        //GiantSpider giantSpider = new GiantSpider();
+        GiantSpider giantSpider = new GiantSpider();
 
-       // Wolf angelawolf = new Wolf( 2);
+        Wolf angelawolf = new Wolf( 2);
 
-       // Action action = new Action(Method.Attack, "Action", 'a', null);
+        Action action = new Action(Method.Attack, "Action", 'a', null);
 
-     //   System.out.println(Method.Flee);
-     //   System.out.println(Method.Attack);
-      //  System.out.println(Method.ViewInventory);
-      //  System.out.println(Method.MoveEast);
-      //  System.out.println(Method.MoveWest);
-       // System.out.println(Method.MoveSouth);
-       // System.out.println(Method.MoveNorth);
-       // System.out.println(Method.Walking);
+        System.out.println(Method.Flee);
+        System.out.println(Method.Attack);
+        System.out.println(Method.ViewInventory);
+        System.out.println(Method.MoveEast);
+        System.out.println(Method.MoveWest);
+        System.out.println(Method.MoveSouth);
+        System.out.println(Method.MoveNorth);
+        System.out.println(Method.Walking);
 
-      //  Flee flee = new Flee();
+        Flee flee = new Flee();
 
-      //  ViewInventory viewinventory = new ViewInventory();
+        ViewInventory viewinventory = new ViewInventory();
         
-      //  MoveEast moveeast = new MoveEast();
-      //  MoveNorth movenorth = new MoveNorth();
-      //  MoveSouth movesouth = new MoveSouth();
-      //  MoveWest movewest = new MoveWest();
-       // Walking walking = new Walking();
+        MoveEast moveeast = new MoveEast();
+        MoveNorth movenorth = new MoveNorth();
+        MoveSouth movesouth = new MoveSouth();
+        MoveWest movewest = new MoveWest();
+        Walking walking = new Walking();
 
         World world = new World();
         world.load_tiles();
